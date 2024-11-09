1f1eded29a90b3f0899a86c132951b73d4db2b2c
David Rubino <drubino@mozilla.com>
2024-11-09 00:08:20 -0800

    Added license information to source files

--- src/content.js
+++ src/content.js
@@ -1,3 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Author: David Rubino
+ */
+
 // Compose a formatted link for the current page
 // Each formatted link has three parts: preText, urlText, and postText.
 // preText: Text to appear before the link. Optional. 
